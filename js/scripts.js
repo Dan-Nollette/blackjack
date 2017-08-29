9956180ae7a7be5e644a19fac5c87143c1130a72
Dan Nollette <nollette.dan@gmail.com>
2017-08-28 17:50:46 -0700

    fix logic in dealHand.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 //BACKEND SCRIPTS (business logic)
 
   //The ranks of cards in a deck and their corresponding values in blackjack.
-var ranks = [["2", 2], ["3", 3], ["4", 4], ["5", 5], ["6", 6], ["7", 7], ["8", 8], ["9", 9], ["10", 10], ["j", 10], ["q", 10], ["k", 10], ["a", 11]];
+var ranks = [["2", 2], ["3", 3], ["4", 4], ["5", 5], ["6", 6], ["7", 7], ["8", 8], ["9", 9], ["10", 10], ["j", 10], ["q", 10], ["k", 10], ["a", 1]];
 
   //The suits of cards in a deck.
 var suits = [["spades", "♠"], ["hearts", "♥"], ["clubs", "♣"], ["diams", "♦"]];
-  //["♠", "♥", "♣", "♦"];
   // BankRoll is the amount of money the player has available to wager.
 var playerBankRoll = 1000;
 
@@ -29,9 +28,10 @@ function Card(rank, suit, suitSymbol, value, number){
   // Individualshand contains an array cards, the score is their combined value, and the number of aces for tracking hard, vs. soft hands. 'Wager' is the number of points the player has bet on this round.
 function IndividualHand(){
   this.cards = [];
+  this.containsAce = false;
   this.hardScore = 0;
   this.softScore = 0;
-  this.wager = 10;
+  this.wager = 25;
 }
 
   //checks if a hand is busted
@@ -45,11 +45,35 @@ IndividualHand.prototype.isBust = function(){
 
   //determines whether the dealer will hit or stay
 var hitOrStay = function(hand){
-  for(; hand.softScore < 17;){
+  for(; hand.hardScore < 17 && hand.softScore < 17;){
     currentShoe.dealCard(hand);
   }
 }
 
+var evaluateRound = function(dealerHand, playerHand) {
+  var dealerFinalScore = dealerHand.finalScore();
+  var playerFinalScore = playerHand.finalScore();
+  if (dealer.isBust()) {
+    //return output for dealer bust. "Dealer is bust, you win!"
+    //add wager to bankroll
+  } else if (playerFinalScore < dealerFinalScore){
+    //return output for player beats dealer "congrats, you win"
+    //add wager to bankroll
+  } else if (playerFinalScore === dealerFinalScore) {
+    //return output for tie "You pushed"
+  } else if (playerFinalScore > dealerFinalScore) {
+    //add wager to bankroll
+    //return output for dealer beats player "sorry, you loose."
+  }
+}
+
+IndividualHand.prototype.finalScore = function(){
+  if (this.softScore > 21) {
+    return this.hardScore;
+  } else {
+    return this.softScore;
+  }
+}
 
   // The shoe object represents all cards in the decks being played. remainingCards are the cards remaining to be dealt, dealtCards are those that have been dealt. redCard is the indicator for when a shuffle is required before the next round can be dealt. decks tracks the number of decks used to create this shoe.
 function Shoe(decks){
@@ -65,6 +89,7 @@ function Shoe(decks){
       })
     })
   }
+  this.shuffle();
 }
 
   //Calling the Shoe.shuffle() method removes all cards from dealtCards and places them in remainingCards. It then randomizes the order of remainingCards.
@@ -81,41 +106,59 @@ Shoe.prototype.shuffle = function(){
   });
 }
 
-  //This method takes a card from remaining cards and adds it to the dealtCards and the hand parameter's cards array, updating hand.score as necessary.
+  //This method takes a card from remaining cards and adds it to the dealtCards and the hand parameter's cards array, updating scores as necessary.
 Shoe.prototype.dealCard = function(hand){
   var currentCard = this.remainingCards.pop();
   hand.cards.push(currentCard);
+  if (currentCard.rank === 'a') {
+    hand.containsAce = true;
+  }
   this.dealtCards.push(currentCard);
   hand.hardScore = hand.hardScore + currentCard.value;
+  hand.softScore = hand.hardScore;
+  if (hand.hardScore < 12 && hand.containsAce){
+    hand.softScore += 10;
+  }
 }
 
 //Deals two cards to the hand parameter
 Shoe.prototype.dealHand = function(individual){
-this.dealCard(individual);
-this.dealCard(individual);
+  individual.cards = [];
+  individual.containsAce = false;
+  individual.hardScore = 0;
+  individual.softScore = 0;
+  individual.wager = 25;
+  this.dealCard(individual);
+  this.dealCard(individual);
 }
 
   //This deals cards to the player and dealer to begin the round, shuffling first if necessary.
 Shoe.prototype.dealRound = function(player, dealer){
-  if (this.redcard >= remainingCards.length){
+  if (this.redcard >= this.remainingCards.length){
     this.shuffle();
   }
   this.dealHand(player);
   this.dealHand(dealer);
+  if(dealer.softScore === 21) {
+    if (player.softScore === 21) {
+      //output something about you and dealer both getting blackjack, "it's a tie"
+    } else {
+      //output something about dealer both getting blackjack, "sorry, you loose"
+    }
+  // display all cards dealt and prompt for starting  next hand
+  }
+  if (player.softScore === 21) {
+    //output something about player getting blackjack, "Blackjack! you get paid 2-1!"
+  }
+  //display player's cards, and a little message about the current score. prompt action.
 }
 
 // takes card as parameter and returns html tags to output for that card
 Card.prototype.toHTML = function(){
-  var rankToPresent = this.rank;
-  console.log(this.rank + "  " + rankToPresent);
-  if (this.rank ==='a' || this.rank ==='k' || this.rank ==='q' || this.rank ==='j') {
-    rankToPresent = this.rank.toUpperCase();
-  }
-  console.log(this.rank + "  " + rankToPresent);
   var returnstring = "";
   returnstring += "<li>"
   returnstring += "\n\t<div class=\"card rank-" + this.rank + " " + this.suit + "\">";
-  returnstring += "\n\t\t<span class=\"rank\">" + rankToPresent + "</span>";
+  returnstring += "\n\t\t<span class=\"rank\">" + (this.rank + "").toUpperCase() + "</span>";
   returnstring += "\n\t\t<span class=\"suit\">" + this.suitSymbol + "</span>";
   returnstring += "\n\t</div>";
   returnstring += "\n</li>"
@@ -125,16 +168,31 @@ Card.prototype.toHTML = function(){
 //FRONTEND SCRIPTS (user interface logic)
 $(document).ready(function(){
 
+  $(".dealButton").click(function(event){
+    event.preventDefault();
+    currentShoe.dealRound(playerHand, dealerHand);
+  });
+
   //scripts for when the player clicks the 'Hit' button
   $(".hitButton").click(function(event){
       event.preventDefault();
-      playerHand(dealCard);
-      //output dealt card, indicate score, possible bust.
+      currentShoe.dealCard(playerHand);
+      //output dealt card, indicate score.
+      if (playerHand.isBust()){
+        //player Bust output
+        //prompt beginning of new hand.
+      } else {
+        //prompt next player action
+      }
   });
+
   //scripts for when the player clicks the 'Stay' button
   $(".stayButton").click(function(event){
       event.preventDefault();
+      //output something about the player standing. Maybe wait for user input before continueing.
       hitOrStay(dealerHand);
+      evaluateRound(dealerHand, playerHand,);
+
       //userPlayer ends their turn. Trigger dealer Turn.
   });
   //scripts for when the player clicks the 'double' button
@@ -161,7 +219,7 @@ $(document).ready(function(){
   // });
 
   //scripts for when the player clicks the 'New Game' button
-  $(".newGameButton").click(function(event){
+  $(".playButton").click(function(event){
       event.preventDefault();
       currentShoe = new Shoe(1);
       playerHand =  new IndividualHand();
@@ -176,18 +234,3 @@ $(document).ready(function(){
       //TO DO: output feedback to let user know the shoe is shuffled
   });
 });
-
-
-
-// // <li>
-// //   <div class="card rank-a spades"><span class="rank">A</span><span class="suit">♠</span></div>
-// // </li>
-// <li>
-//           <div class="card rank-a diams"><span class="rank">A</span><span class="suit">♦</span></div>
-//         </li>
-//         <li>
-//         	<div class="card rank-a diamonds">
-//         		<span class="rank">a</span>
-//         		<span class="suit">♦</span>
-//         	</div>
-//         </li>
